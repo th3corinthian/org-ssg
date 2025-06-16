0ad1c593c74301ffbf263420fd9d4231aa845491
corinthian <corinthian@nixos.com>
2025-06-15 21:47:24 -0700

    updated cli args

--- cmd/new.go
+++ cmd/new.go
@@ -5,25 +5,19 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
 	"github.com/th3corinthian/org-ssg/internal/new"
 	"github.com/spf13/cobra"
 )
 
 // newCmd represents the new command
 var newCmd = &cobra.Command{
-	Use:   "new",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "new [project_name]",
+	Short: "Generate a new project directory",
+	Long: `Input desired name for project, 'new' will auto generate your workspace`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("new called")
-		new.NewProj("Hello")
-		new.GenConfig("Hello")
+		path := args[0]
+		new.NewProj(path)
+		new.GenConfig(path)
 	},
 }
 
