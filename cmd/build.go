56798363d9a697e3342501c0e8e5355bc3cab24f
corinthian <corinthian@nixos.com>
2025-06-22 16:38:15 -0700

    added build command

---
+++ cmd/build.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/th3corinthian/org-ssg/internal/build"
+	"github.com/spf13/cobra"
+)
+
+// buildCmd represents the build command
+var buildCmd = &cobra.Command{
+	Use:   "build",
+	Short: "build and generate the files needed to deploy",
+	Long: `generates HTML files from specified org-mode files`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("build called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(buildCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// buildCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// buildCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ internal/build/build_base.go
@@ -0,0 +1,79 @@
+package build
+
+import (
+	"io/ioutil"
+	"net/http"
+	"log"
+	"fmt"
+	"os"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host		string		`yaml:"host"`
+		Port		int			`yaml:"port"`
+		UseTLS		bool		`yaml:"use_tls"`
+	} `yaml:"server"`
+	Paths struct {
+		Input		string		`yaml:"input"`
+		Output		string		`yaml:"output"`
+		Assets		string		`yaml:"assets"`
+		Templates	string		`yaml:"templates"`
+	} `yaml:"paths"`
+}
+
+func loadConfig(filepath string) (*Config, error) {
+	data, err := ioutil.Readfile(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+func configServer() {
+	cfg, err := loadConfig("myproj/config.yml")
+	if err != nil {
+		log.Fatalf("[-] Error loading config: %v", err)
+	}
+
+	// create a server with the configuration values
+	serverAddr := fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)
+
+	if cfg.Server.UseTLS {
+		log.Printf("[+] Starting server with TLS on %s", serverAddr)
+		err := http.ListenAndServeTLS(serverAddr, "cert.pem", "key.pem", nil)
+		if err != nil {
+			log.Fatalf("[-] Error starting TLS server: %v", err)
+		}
+	} else {
+		log.Printf("[+] Starting server on %s", serverAddr)
+		err := http.ListenAndServe(serverAddr, nil)
+		if err != nil {
+			log.Fatalf("[-] Error starting server: %v", err)
+		}
+	}
+}
+
+func serverHandle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Server settings: Host - %s, Port - %d", r.Host , 8080)
+}
+
+func pathsHandle(w http.ResponseWriter, r *http.Request) {}
+
+var handlers = map[string]http.HandlerFunc{
+	"host": 		serverHandle,
+	"port": 		serverHandle,
+	"use_tls:":		serverHandle,
+
+	"input":		pathsHandle,
+	"output":		pathsHandle,
+	"assets":		pathsHandle,
+	"templates":	pathsHandle,
+}
